1b9428811e761679319e1ddc61fc29f731448c13
Marcos Paulo <dev.marcospereira@gmail.com>
2022-08-19 10:17:28 -0300

    Menu Lateral com efeitos

--- portfolio/src/style/ToolbarTitleStyle.js
+++ portfolio/src/style/ToolbarTitleStyle.js
@@ -3,4 +3,24 @@ import { styled, Typography } from "@mui/material";
 
 
 export const TypographyCustom = styled(Typography)`
+  border-right: 2px solid;
+  animation: blinkCursor 500ms steps(30) infinite normal, typing 1s steps(15) 1s normal both;
+  overflow: hidden;
+  @keyframes typing {
+    from {
+      width: 0;
+    }
+    to {
+      width: 6em;
+    }
+  }
+
+  @keyframes blinkCursor {
+    from {
+      border-right-color: ${({theme}) => theme.palette.common.black };
+    }
+    to {
+      border-right-color: transparent;
+    }
+  }
 `
