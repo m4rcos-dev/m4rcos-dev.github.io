2f54165b93ee4ec39e4929a88a7e8f764d280b09
Marcos Paulo <dev.marcospereira@gmail.com>
2022-08-22 11:12:06 -0300

    Personaliza NavAppBar e NavDrawer

--- portfolio/src/components/NavAppBar.js
+++ portfolio/src/components/NavAppBar.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Box from '@mui/material/Box';
 import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import Menu from '@mui/material/Menu';
 import MenuIcon from '@mui/icons-material/Menu';
 import MenuItem from '@mui/material/MenuItem';
+import HomeIcon from '@mui/icons-material/Home';
+import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
+import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
+import { styled } from "@mui/material";
 
-const pages = ['Products', 'Pricing', 'Blog'];
+const arrayNavLinksText = ['Inicio', 'Sobre', 'Projetos'];
+const arrayIcons = [
+  <HomeIcon fontSize="medium" />,
+  <AssignmentIndIcon fontSize="medium" />,
+  <SettingsEthernetIcon fontSize="medium" />
+];
+
+const MenuItemCustom = styled(MenuItem)`
+${({ theme }) => `
+cursor: crosshair;
+&:hover {
+  color: ${theme.palette.alternateColor1.main};
+}
+`}
+`
 
 const NavAppBar = () => {
   const [anchorElNav, setAnchorElNav] = React.useState(null);
@@ -18,44 +35,45 @@ const NavAppBar = () => {
   const handleCloseNavMenu = () => {
     setAnchorElNav(null);
   };
-    return (
-      <Box sx={{ display: 'flex'}}>
-        <IconButton
-          size="large"
-          aria-label="account of current user"
-          aria-controls="menu-appbar"
-          aria-haspopup="true"
-          onClick={handleOpenNavMenu}
-          color="inherit"
-        >
-          <MenuIcon />
-        </IconButton>
-        <Menu
-          id="menu-appbar"
-          anchorEl={anchorElNav}
-          anchorOrigin={{
-            vertical: 'bottom',
-            horizontal: 'left',
-          }}
-          keepMounted
-          transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-          }}
-          open={Boolean(anchorElNav)}
-          onClose={handleCloseNavMenu}
-          sx={{
-            display: { xs: 'block', md: 'none' },
-          }}
-        >
-          {pages.map((page) => (
-            <MenuItem key={page} onClick={handleCloseNavMenu}>
-              <Typography textAlign="center">{page}</Typography>
-            </MenuItem>
-          ))}
-        </Menu>
-      </Box>
-    )
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <IconButton
+        size="large"
+        aria-label="account of current user"
+        aria-controls="menu-appbar"
+        aria-haspopup="true"
+        onClick={handleOpenNavMenu}
+        color="inherit"
+      >
+        <MenuIcon sx={{ color: 'alternateColor1.main'}}/>
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorElNav}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        keepMounted
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        open={Boolean(anchorElNav)}
+        onClose={handleCloseNavMenu}
+        sx={{
+          display: { xs: 'block', md: 'none' },
+        }}
+      >
+        {arrayNavLinksText.map((page, index) => (
+          <MenuItemCustom key={page} onClick={handleCloseNavMenu}>
+              {arrayIcons[index]}
+              {page}
+          </MenuItemCustom>
+        ))}
+      </Menu>
+    </Box>
+  )
 }
 
 export default NavAppBar;
--- portfolio/src/components/NavDrawer.js
+++ portfolio/src/components/NavDrawer.js
@@ -1,11 +1,10 @@
 import React from "react";
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import HomeIcon from '@mui/icons-material/Home';
 import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import SettingsEthernetIcon from '@mui/icons-material/SettingsEthernet';
-import { Box, styled, Typography } from "@mui/material";
+import { Box, styled } from "@mui/material";
 import { Link } from 'react-router-dom';
 
 class NavDrawer extends React.Component {
@@ -13,21 +12,15 @@ class NavDrawer extends React.Component {
     const ListemItemButtonCustom = styled(ListItemButton)`
     ${({ theme }) => `
     cursor: crosshair;
+    display: flex;
+    flex-direction: column;
     justify-content: center;
     &:hover {
       color: ${theme.palette.alternateColor1.main};
     }
     `}
     `
-    const TypographyCustom = styled(Typography)`
-    ${({ theme }) => `
-    cursor: crosshair;
-    justify-content: center;
-    &:hover {
-      color: ${theme.palette.alternateColor1.main};
-    }
-    `}
-    `
+
     const arrayNavLinksText = ['Inicio', 'Sobre', 'Projetos'];
     const arrayNavLinks = ['/', '/about', '/projects'];
     const arrayIcons = [
@@ -40,18 +33,12 @@ class NavDrawer extends React.Component {
       <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <List sx={{ mb: '3rem', mt: '9rem', }}>
           {arrayNavLinksText.map((text, index) => (
-            <ListItem key={text}>
-
-              <ListemItemButtonCustom divider>
-                <Link to={arrayNavLinks[index]} style={{ textDecoration: 'none', color: 'inherit' }}>
-                  <ListemItemButtonCustom>
-                    {arrayIcons[index]}
-                  </ListemItemButtonCustom>
-                  <TypographyCustom variant="h5" fontFamily='Hack'>{text}</TypographyCustom>
-                </Link>
+            <Link to={arrayNavLinks[index]} style={{ textDecoration: 'none', color: 'inherit' }}>
+              <ListemItemButtonCustom divider sx={{ fontFamily: 'Hack', fontSize: '1.5rem', mb: '1rem' }}>
+                {arrayIcons[index]}
+                {text}
               </ListemItemButtonCustom>
-
-            </ListItem>
+            </Link>
           ))}
         </List>
       </Box >
