e38eb1bbfd450de08b0b5a8819db27593d98136e
Marcos Paulo <dev.marcospereira@gmail.com>
2022-08-23 13:03:13 -0300

    Adicona Component ColorChange, ajusta posição ControlPanel

--- portfolio/src/components/ColorChange.js
+++ portfolio/src/components/ColorChange.js
@@ -1,38 +1,36 @@
 import * as React from 'react';
 import styled from "@emotion/styled";
-import { Box, SpeedDial, SpeedDialAction, SpeedDialIcon } from "@mui/material";
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
-
+import { Box, SpeedDial, SpeedDialAction } from "@mui/material";
+import ColorLensIcon from '@mui/icons-material/ColorLens';
+import CircleIcon from '@mui/icons-material/Circle';
 
 const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
-  // position: 'absolute',
-  '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-  '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
-    top: theme.spacing(2),
-    left: theme.spacing(2),
-  },
+  // color: theme.palette.primary.dark,
+  // '&.MuiSpeedDial-fab': {
+  //   bgcolor: theme.palette.primary.light,
+  //   // bottom: theme.spacing(2),
+  //   // right: theme.spacing(2),
+  // },
+  // // '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
+  // //   bgcolor: theme.palette.secondary.dark,
+  // //   // top: theme.spacing(2),
+  // //   // left: theme.spacing(2),
+  // // },
 }));
 
 const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
+  { icon: <CircleIcon />, name: 'Color1', color: 'alternateColor1.main' },
+  { icon: <CircleIcon />, name: 'Color2', color: 'alternateColor1.main'},
+  { icon: <CircleIcon />, name: 'Color3', color: 'alternateColor1.main' },
+  { icon: <CircleIcon />, name: 'Color4', color: 'alternateColor1.main' },
 ];
 
 function ColorChange() {
   return(
-    <Box>
+    <Box sx={{ mt: '1rem', mr: '1rem'}}>
        <StyledSpeedDial
           ariaLabel="SpeedDial playground example"
-          // hidden
-          icon={<SpeedDialIcon />}
+          icon={<ColorLensIcon />}
           direction='left'
         >
           {actions.map((action) => (
@@ -40,6 +38,7 @@ function ColorChange() {
               key={action.name}
               icon={action.icon}
               tooltipTitle={action.name}
+              sx={{ color: `${action.color}`, bgcolor: `${action.color}`}}
             />
           ))}
         </StyledSpeedDial>
--- portfolio/src/components/ControlPanel.js
+++ portfolio/src/components/ControlPanel.js
@@ -6,7 +6,14 @@ import ThemeChange from "./ThemeChange";
 class ControlPanel extends React.Component {
   render(){
     return(
-      <Box sx={{ position: 'Fixed', right: '0', mt: '5rem', mr: '0.5rem' }}>
+      <Box sx={{
+        position: 'Fixed',
+        right: '0',
+        mt: '5rem', mr: '0.5rem',
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'flex-end',
+        }}>
           <ThemeChange />
           <ColorChange />
       </Box>
