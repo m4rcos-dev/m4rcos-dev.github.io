89633ece52e0f262bd164088736c4695ba9d0e58
Marcos Paulo <dev.marcospereira@gmail.com>
2022-08-24 15:00:45 -0300

    Configura ChangeTheme Background AppBarTop

--- portfolio/src/components/AppBarTop.js
+++ portfolio/src/components/AppBarTop.js
@@ -5,6 +5,7 @@ import NavSoicalMedia from "./NavSocialMedia";
 import NavAppBar from "./NavAppBar";
 import { styled } from "@mui/material";
 import ToolbarTitleSmall from "./ToolbarTitleSmall";
+import { connect } from "react-redux";
 
 //=============Breackpoints MediaQuery==================
 const BoxCustom = styled('div')(({ theme }) => ({
@@ -13,28 +14,40 @@ const BoxCustom = styled('div')(({ theme }) => ({
   },
 }));
 
-const AppBarTop = () => {
-  return (
-    <BoxCustom>
+class AppBarTop extends React.Component {
+  render() {
+    const { currentTheme } = this.props;
+    return (
+      <BoxCustom>
 
-      <Box sx={{
-        width: '100%',
-        height: '8vh',
-        position: 'Fixed',
-        bgcolor: 'primary.main',
-        borderBottom: 1,
-        borderColor: 'divider',
-        display: 'flex',
-        justifyContent: 'space-between'
-      }}>
-        <NavAppBar />
-        <Toolbar />
-        <ToolbarTitleSmall />
-        <NavSoicalMedia />
-      </Box>
+        <Box sx={{
+          width: '100%',
+          height: '8vh',
+          position: 'Fixed',
+          bgcolor: `primary.${currentTheme}`,
+          borderBottom: 1,
+          borderColor: 'divider',
+          display: 'flex',
+          justifyContent: 'space-between'
+        }}>
+          <NavAppBar />
+          <Toolbar />
+          <ToolbarTitleSmall />
+          <NavSoicalMedia />
+        </Box>
 
-    </BoxCustom>
-  );
+      </BoxCustom>
+    );
+  }
 };
 
-export default AppBarTop;
+// =======props Redux===============================
+const mapStateToProps = (state) => ({
+  ...state.themeChange,
+});
+
+
+// export default AppBarTop;
+
+//============export Redux==========================
+export default connect(mapStateToProps)(AppBarTop);
