9efc0110eb03a30fb5c53ca31b2f539715523264
Marcos Paulo <dev.marcospereira@gmail.com>
2022-08-24 16:06:19 -0300

    Configura ChangeTheme ContentAppBarTop

--- portfolio/src/components/ContentAppBarTop.js
+++ portfolio/src/components/ContentAppBarTop.js
@@ -3,9 +3,11 @@ import React from "react";
 import Home from "../pages/Home";
 import About from "../pages/About";
 import Projects from "../pages/Projects";
+import { connect } from "react-redux";
 
 class ContentAppBarTop extends React.Component {
   render() {
+    const { currentTheme } = this.props;
     //=============Breackpoints MediaQuery==================
     const BoxCustom = styled('div')(({ theme }) => ({
       [theme.breakpoints.up('md3')]: {
@@ -18,21 +20,21 @@ class ContentAppBarTop extends React.Component {
         <Box id='home' sx={{
           width: '100%',
           height: '92vh',
-          bgcolor: 'secondary.main',
+          bgcolor: `secondary.${currentTheme}`,
         }}>
           <Home />
         </Box>
         <Box id='about' sx={{
           width: '100%',
           height: '92vh',
-          bgcolor: 'secondary.main',
+          bgcolor: `secondary.${currentTheme}`
         }}>
           <About />
         </Box>
         <Box id='projects' sx={{
           width: '100%',
           height: '92vh',
-          bgcolor: 'secondary.main',
+          bgcolor: `secondary.${currentTheme}`
         }}>
           <Projects />
         </Box>
@@ -41,4 +43,12 @@ class ContentAppBarTop extends React.Component {
   }
 }
 
-export default ContentAppBarTop;
+// =======props Redux===============================
+const mapStateToProps = (state) => ({
+  ...state.themeChange,
+});
+
+// export default ContentAppBarTop;
+
+//============export Redux==========================
+export default connect(mapStateToProps)(ContentAppBarTop);
